1d3194e13cf55e9dc307dfab2f80dd2caed54832
tailorraj <tailorraj111@gmail.com>
2024-05-21 08:35:32 +0000

    sales visit creation issue

--- hrms/hr/doctype/field_report/field_report.py
+++ hrms/hr/doctype/field_report/field_report.py
@@ -4,8 +4,13 @@
 import frappe
 from frappe.model.document import Document
 from hrms.api import get_location_for_lat_lng
-
+from frappe import _
 
 class FieldReport(Document):
 	def validate(self):
-		self.place_name = get_location_for_lat_lng(self.get("latitude") , self.get("longitude")).get('display_name') or ""
+		try:
+			self.place_name = get_location_for_lat_lng(self.get("latitude") , self.get("longitude")).get('display_name') or ""
+		except Exception as e:
+			
+			frappe.log_error(frappe.get_traceback(), _("Field Report Error"))
+			frappe.msgprint(_("Not Able to fetch location name. Please check the permission for the location."))
